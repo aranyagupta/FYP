1670e8fb3136570d86cecf47fead2b082f973fa1
Aranya <ag1421@ic.ac.uk>
2025-04-29 23:59:58 +0100

    Increase scheduler steps Decrease depths

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ DISPLAY_SYMBOLIC = False
 PLOT_GRAPHS = False
 
 if __name__ == "__main__":
-    min_hidden_layers = 3
-    max_hidden_layers = 3
+    min_hidden_layers = 2
+    max_hidden_layers = 2
     min_layer_width = 4
     max_layer_width = 6
 
