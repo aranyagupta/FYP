abad2279cdd4ff011ba68e9f29ee425f5b4f4ea8
Aranya <ag1421@ic.ac.uk>
2025-04-29 22:55:43 +0100

    fix bug

--- WitsEnv.py
+++ WitsEnv.py
@@ -294,7 +294,7 @@ class WitsEnvLSA:
             integrand = (2*(current_x_1-x_2) + (y_1-current_x_1) * (current_x_1-x_2)**2)*f_X(current_x_0)*f_W(y_1-current_x_1)
             # computing integral over all y_1
             integral = torch.trapz(y=integrand[indices].reshape(integrand.shape[0], 1), x=y_1_integrating, dim=0)
-            dJ_dx1_check[i] = dJ_dx1[i] + integral
+            dJ_dx1_check[i] = dJ_dx1_check[i] + integral
         assert torch.any(torch.abs(dJ_dx1_check - dJ_dx1)<=1e-4), f"dJ_dx1 is not accurate:\n value={dJ_dx1},\n check={dJ_dx1_check}"
         ############################################################################
 
@@ -313,7 +313,7 @@ class WitsEnvLSA:
             current_x_1 = x_1[i]
             integrand = ((y_1-current_x_1)*(current_x_1-x_2+2)**2 - (current_x_1-x_2))**2 * f_X(current_x_0)*f_W(y_1-current_x_1)
             integral = torch.trapz(integrand[indices].reshape(integrand.shape[0], 1), y_1_integrating, dim=0)
-            dx1_dJ_dx1_check[i] = dx1_dJ_dx1[i] + integral
+            dx1_dJ_dx1_check[i] = dx1_dJ_dx1_check[i] + integral
         assert torch.any(torch.abs(dx1_dJ_dx1_check - dx1_dJ_dx1)<=1e-4), f"dx1_dJ_dx1 is not accurate:\n value:{dx1_dJ_dx1},\n check:{dx1_dJ_dx1_check}"
         ############################################################################
         # print("dJ_dx1 has nan:", torch.any(torch.isnan(dJ_dx1)))
