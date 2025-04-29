279de0b975f7f4ffa74d8301622c5a1ad816e81e
Aranya <ag1421@ic.ac.uk>
2025-04-29 21:37:19 +0100

    add shape printouts

--- WitsPPO.py
+++ WitsPPO.py
@@ -481,7 +481,9 @@ class WitsLSA:
 			for i in range(self.N):
 				dJ_dx1, dx1_dJ_dx1, out, x_0 = self.env.step_timesteps(self.actor_c1, self.actor_c2, timesteps)
 				gradients = dJ_dx1/torch.abs(dx1_dJ_dx1) 
+				print("gradients.shape", gradients.shape)
 				small_mask = torch.abs(dx1_dJ_dx1) <= 1e-6  
+				print("small_mask.shape", small_mask.shape)
 				gradients[small_mask] = self.tau * dJ_dx1[small_mask]
 				count = small_mask.sum().item()
 				########## FOR LOOP IMPLEMENTATION - SLOW ####################
