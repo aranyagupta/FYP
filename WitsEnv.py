fix sigma range reduce precision to 1e-4

WitsEnv.py
@@ -258,10 +258,10 @@ class WitsEnvLSA:
         # y_1 = x_1 + w
         # x_2 = self.generate_u1_tensor(y_1, x_1)
 
-        x_0 = torch.arange(-self.sigma, self.sigma, (2*self.sigma)/timesteps)
+        x_0 = torch.arange(-3*self.sigma, 3*self.sigma, (6*self.sigma)/timesteps)
         x_0 = x_0.reshape(x_0.shape[0], 1)
         x_1 = actor_c1(x_0)
-        y_1 = torch.arange(-self.sigma, self.sigma, (2*self.sigma)/timesteps)
+        y_1 = torch.arange(-3*self.sigma, 3*self.sigma, (6*self.sigma)/timesteps)
         y_1 = y_1.reshape(y_1.shape[0], 1)
         x_2 = self.generate_u1_tensor(y_1, x_1)
 