7b5af78b8c77dd27fdb485bd0af41892e25eca96
Aranya <ag1421@ic.ac.uk>
2025-05-13 23:16:42 +0100

    run FGD models

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ Training configurations
 """
 configs = [
     {
-        "kvals":[0.1, 0.2, 0.3, 0.4, 0.5],
-        "sigvals":[5.0],
+        "kvals":[0.22, 0.32, 0.39, 0.45, 0.50, 0.55, ],
+        "sigvals":[2.24, 3.16, 3.87, 4.47, 5.00, 5.48, 5.92, 6.32],
         "kanHyps":[[1,10,1],[1,11,1],[1,12,1]],
         "kanType":kan.KAN,
-        "env":WitsEnv.WitsEnvLSA,
-        "trainer":WitsPPO.WitsLSA,
+        "env":WitsEnv.WitsEnvFGD,
+        "trainer":WitsPPO.WitsFGD,
         "modelType":"LSA",
         "prefit_func_1": lambda sigma, x : sigma*torch.sign(x),
         "prefit_func_2": None,
