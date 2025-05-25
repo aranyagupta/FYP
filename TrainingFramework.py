efee9cd764e19170579fe74e5a7ce0d7d4dc73fb
Aranya <ag1421@ic.ac.uk>
2025-05-25 19:56:39 +0100

    verify that first outputs are linear

--- TrainingFramework.py
+++ TrainingFramework.py
@@ -89,6 +89,13 @@ class TrainingFramework:
                         actor_c1 = prefit_model_1.copy()
                     if prefit_func_2 is not None:
                         actor_c2 = prefit_model_2.copy()
+
+                    with torch.no_grad():
+                        domain = torch.linspace(-15.0, 15.0, 1000, device=self.env.device).reshape(1000, 1)
+                        m1_out = self.actor_c1(domain)
+                        m2_out = self.actor_c2(domain)
+                        print(f"tf m1_out: {m1_out}")
+                        print(f"tf m2_out: {m2_out}")
                     trainEnv = env(k, sigma, device=self.device, mode='TRAIN')
                     alg = trainer(trainEnv, actor_c1, actor_c2, lr)
                     
--- WitsPPO.py
+++ WitsPPO.py
@@ -212,8 +212,6 @@ class WitsMomentum(WitsTrainer):
 			raise Exception("Changed timesteps in between training - not allowed!")
 		
 		for batch in range(batches):
-			gradient_1, gradient_2, out_1, out_2, J = self.env.step_timesteps(self.actor_c1, self.actor_c2, timesteps=timesteps, zeta_1=self.zeta_1, zeta_2=self.zeta_2)
-			
 			if batch % 50 == 0 and self.env.k == 0.20 and self.env.sigma == 5.00:
 				with torch.no_grad():
 					domain = torch.linspace(-15.0, 15.0, 1000, device=self.env.device).reshape(1000, 1)
@@ -221,6 +219,12 @@ class WitsMomentum(WitsTrainer):
 					m2_out = self.actor_c2(domain)
 					torch.save(m1_out, f"batch-{batch}-m1.pt")
 					torch.save(m2_out, f"batch-{batch}-m2.pt")
+					print(f"batch:, {batch}")
+					print(f"m1_out: {m1_out}")
+					print(f"m2_out: {m2_out}")
+			
+			gradient_1, gradient_2, out_1, out_2, J = self.env.step_timesteps(self.actor_c1, self.actor_c2, timesteps=timesteps, zeta_1=self.zeta_1, zeta_2=self.zeta_2)
+			
 
 			self.actor_c1_optim.zero_grad()
 			self.actor_c2_optim.zero_grad()
