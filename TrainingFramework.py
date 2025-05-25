53efa0d7c471a45e13ae40fbe52d6edf5835acc1
Aranya <ag1421@ic.ac.uk>
2025-05-25 20:02:25 +0100

    bug fix

--- TrainingFramework.py
+++ TrainingFramework.py
@@ -91,9 +91,9 @@ class TrainingFramework:
                         actor_c2 = prefit_model_2.copy()
 
                     with torch.no_grad():
-                        domain = torch.linspace(-15.0, 15.0, 1000, device=self.env.device).reshape(1000, 1)
-                        m1_out = self.actor_c1(domain)
-                        m2_out = self.actor_c2(domain)
+                        domain = torch.linspace(-15.0, 15.0, 1000, device=self.device).reshape(1000, 1)
+                        m1_out = actor_c1(domain)
+                        m2_out = actor_c2(domain)
                         print(f"tf m1_out: {m1_out}")
                         print(f"tf m2_out: {m2_out}")
                     trainEnv = env(k, sigma, device=self.device, mode='TRAIN')
